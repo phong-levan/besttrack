179d8fab89e03b64b63e1526766a4484e5905095
Lê Văn Phong <106429839+phong-levan@users.noreply.github.com>
2026-02-09 14:39:21 +0700

    Update ve.py

--- ve.py
+++ ve.py
@@ -457,7 +457,7 @@ def main():
                         width: 100%; 
                         height: 1000px; 
                         position: absolute; 
-                        top: -55px;     /* Kéo lên để ẩn Header màu đen */
+                        top: -50px;     /* Kéo lên để ẩn Header màu đen */
                         left: 0px; 
                         border: none;"
                     allow="fullscreen"
@@ -577,6 +577,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
